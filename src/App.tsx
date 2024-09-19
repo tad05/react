4a1d76eb23fb37194a3827abf1b4f2470a95f36b
tad05 <tad05@naver.com>
2024-09-19 10:19:17 +0900

    react

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+function App() {
+  const [wedding, setWedding] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    fetch('http://localhost:8888/weddings')
+      .then((response) => {
+        if (response.ok === false) {
+          throw new Error('청첩장 정보를 불러오지 못했습니다.')
+        }
+        return response.json()
+      })
+      .then((data) => {
+        setWedding(data)
+      })
+      .catch(() => {
+        setError(true)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) {
+    return <FullScreenMessage type="loading" />
+  }
+
+  if (error) {
+    return <FullScreenMessage type="error" />
+  }
+
+  return <div className={cx('container')}>{JSON.stringify(wedding)}</div>
+}
+export default App
